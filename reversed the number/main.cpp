fe48becf16952bdbdbaa407f52b9ee1251747b02
Marwan Khalid <33214208+marwankhalid@users.noreply.github.com>
2021-01-06 22:39:07 +0500

    Done

--- reversed the number/main.cpp
+++ reversed the number/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int number,remainder,reversedNumber=0;
+    while (true) {
+        cout<<"Enter the any numbers\t";
+        cin>>number;
+        if (number == 0) {
+            return 0;
+            break;
+        }
+        while (number!=0) {
+            remainder = number % 10;
+            reversedNumber = (reversedNumber * 10) + remainder;
+            number /= 10;
+        }
+        cout<<"Reversed Number = "<<reversedNumber<<"\n";
+        reversedNumber = 0;
+    }
+    
+    
     return 0;
 }
